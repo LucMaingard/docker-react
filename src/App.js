e9fd951e0d84b608dd5f063a8a2eb54d2e8881bd
LucMaingard <luc.ma7154@gmail.com>
2023-01-13 11:40:25 +0200

    checked app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hello motherfucker 
+        WooHoo :):
         </p>
         <a
           className="App-link"
